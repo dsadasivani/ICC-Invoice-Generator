bfc0968234fd4f9bfded19998daaf4398ba5d9dc
Dilip Kumar Patnaik Sadasivani <dsadasivani@github.com>
2020-07-10 20:52:55 +0530

    First Draft

---
+++ ITextDemo/src/main/java/com/demo/ITextDemo/Solution.java
@@ -0,0 +1,28 @@
+package com.demo.ITextDemo;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+       
+        double payment = 48505.08;
+
+        // Write your code here.
+        NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat cnFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat frFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        NumberFormat inFormat = NumberFormat.getCurrencyInstance(new Locale("en", "in"));
+
+        String us = usFormat.format(payment);
+        String india = inFormat.format(payment);
+        String france = frFormat.format(payment);
+        String china = cnFormat.format(payment);
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
